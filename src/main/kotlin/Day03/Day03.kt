55525dc0b08fda73a46bc3f7eaedd4b12ea87ac7
Ivan <ivancordonm@gmail.com>
2023-12-03 20:24:37 +0100

    Day 03. part 2

--- src/main/kotlin/Day03/Day03.kt
+++ src/main/kotlin/Day03/Day03.kt
@@ -12,6 +12,9 @@ fun main() {
     check(part1(testInput) == 4361)
     println(part1(input))
 
+    check(part2(testInput) == 467835)
+    println(part2(input))
+
 }
 
 fun part1(board: List<String>): Int {
@@ -31,4 +34,17 @@ fun isPartNumber(board: List<String>, index: Int, number: List<Int>): Boolean {
     val y = max(number.first() - 1, 0) to min(number.last() + 1, board[0].lastIndex)
     val subBoard = board.subList(x.first, x.second + 1).map { it.substring(y.first, y.second + 1) }
     return subBoard.any { it.any { it.toString().matches(Regex("[^\\.\\d]")) } }
+}
+
+fun part2(board: List<String>) =
+    board.foldIndexed(0) { index, acc, s ->
+        acc + Regex("(\\*)").findAll(s).map { gearRatio(board, index, it.range.first) }.sum()
+    }
+
+
+fun gearRatio(board: List<String>, row: Int, col: Int): Int {
+    val subBoard = board.subList(max(row - 1, 0), min(row + 1, board.lastIndex) + 1)
+    val numbers = subBoard.flatMap { "(\\d+)".toRegex().findAll(it).toList() }
+    val gears = numbers.filter { it.range.intersect(col - 1..col + 1).isNotEmpty() }.map { it.value.toInt() }
+    return if (gears.count() > 1) gears.reduce { acc, i -> acc * i } else 0
 }
